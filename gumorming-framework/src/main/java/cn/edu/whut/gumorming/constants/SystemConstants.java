ab343692aab5b2817ce9609c676e6bf835a440a6
GuMorming <gumorming@163.com>
2023-07-06 21:38:04 +0800

    常量

---
+++ gumorming-framework/src/main/java/cn/edu/whut/gumorming/constants/SystemConstants.java
@@ -0,0 +1,21 @@
+package cn.edu.whut.gumorming.constants;
+
+/**
+ * @author : GuMorming
+ * @Project : GuMormingBlog
+ * @Package : cn.edu.whut.gumorming.constants
+ * @createTime : 2023/7/6 21:02
+ * @Email : gumorming@163.com
+ * @Description :
+ */
+
+public class SystemConstants {
+    /**
+     * 文章是草稿
+     */
+    public static final int ARTICLE_STATUS_DRAFT = 1;
+    /**
+     * 文章是正常发布状态
+     */
+    public static final int ARTICLE_STATUS_NORMAL = 0;
+}
