a9e3b7c25f452add2a721b201c924ad0459b8b22
GuMorming <gumorming@163.com>
2023-07-07 18:16:11 +0800

    自定义异常处理

---
+++ gumorming-framework/src/main/java/cn/edu/whut/gumorming/handler/security/AccessDeniedHandlerImpl.java
@@ -0,0 +1,25 @@
+package cn.edu.whut.gumorming.handler.security;
+
+import cn.edu.whut.gumorming.domain.ResponseResult;
+import cn.edu.whut.gumorming.domain.enums.AppHttpCodeEnum;
+import cn.edu.whut.gumorming.utils.WebUtils;
+import com.alibaba.fastjson.JSON;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        accessDeniedException.printStackTrace();
+        ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NO_OPERATOR_AUTH);
+        //响应给前端
+        WebUtils.renderString(response, JSON.toJSONString(result));
+    }
+}
---
+++ gumorming-framework/src/main/java/cn/edu/whut/gumorming/handler/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,44 @@
+package cn.edu.whut.gumorming.handler.security;
+
+import cn.edu.whut.gumorming.domain.ResponseResult;
+import cn.edu.whut.gumorming.domain.enums.AppHttpCodeEnum;
+import cn.edu.whut.gumorming.utils.WebUtils;
+import com.alibaba.fastjson.JSON;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author : GuMorming
+ * @Project : GuMormingBlog
+ * @Package : cn.edu.whut.gumorming.handler.security
+ * @createTime : 2023/7/7 18:14
+ * @Email : gumorming@163.com
+ * @Description :
+ */
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        authException.printStackTrace();
+        //InsufficientAuthenticationException
+        //BadCredentialsException
+        ResponseResult result = null;
+        if (authException instanceof BadCredentialsException) {
+            result = ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_ERROR.getCode(), authException.getMessage());
+        } else if (authException instanceof InsufficientAuthenticationException) {
+            result = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+        } else {
+            result = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(), "认证或授权失败");
+        }
+        //响应给前端
+        WebUtils.renderString(response, JSON.toJSONString(result));
+    }
+}
