14fffcfd189f91757b6bbc5bde3d544cb898b8cc
GuMorming <gumorming@163.com>
2023-07-08 22:56:16 +0800

    实现日志AOP打印输出

--- gumorming-blog/src/main/java/cn/edu/whut/gumorming/GuMormingBlogApplication.java
+++ gumorming-blog/src/main/java/cn/edu/whut/gumorming/GuMormingBlogApplication.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @MapperScan("cn.edu.whut.gumorming.mapper")
 @PropertySource("classpath:application-db.properties")
+@PropertySource("classpath:application-qiniu.properties")
 public class GuMormingBlogApplication {
     public static void main(String[] args) {
         SpringApplication.run(GuMormingBlogApplication.class, args);
